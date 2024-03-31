61d3fb019af58ca86f26c08a2eefe49f24076edf
Vasilis Papagrigoriou <vasilispapg@outlook.com>
2024-03-31 19:16:41 +0300

    normalize and resize frames

--- DataExtraction/frames.py
+++ DataExtraction/frames.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 def extract_frames(video_path, frame_rate=15):
     video = cv2.VideoCapture(video_path)
@@ -9,7 +10,11 @@ def extract_frames(video_path, frame_rate=15):
     while success:
         success, image = video.read()
         if count % frame_rate == 0 and success:
+            # resize image to 299x299
+            image = ((image - image.min()) / (image.max() - image.min())).astype(np.float32)
+            image = cv2.resize(image, (299, 299))
             frames.append(image)
+            
         count += 1
 
     video.release()
