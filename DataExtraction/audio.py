c9f44846de3bbbb77705687918840158ebda1ed1
Vasilis Papagrigoriou <vasilispapg@outlook.com>
2024-03-31 19:16:14 +0300

    changed the shape of audio

--- DataExtraction/audio.py
+++ DataExtraction/audio.py
@@ -21,21 +21,20 @@ def extract_audio_from_video(video_path, audio_output_path):
     
 def extract_audio_features_for_each_frame(audio_output_path: str, num_frames: int):
     """
-    The function `extract_audio_features_for_each_frame` extracts MFCC features for each frame of an
-    audio file based on a specified number of frames.
+    This function extracts audio features, specifically MFCCs, for each frame of a video by
+    processing the audio data.
     
-    :param audio_output_path: The `audio_output_path` parameter is a string that represents the file
-    path to the audio file from which you want to extract audio features. This function loads the audio
-    file using librosa and then extracts MFCC (Mel-frequency cepstral coefficients) features for each
-    frame of the audio
-    :type audio_output_path: str
-    :param num_frames: The `num_frames` parameter represents the number of frames you want to divide the
-    audio into for feature extraction. This value determines how many segments the audio will be divided
-    into, with each segment processed to extract MFCCs (Mel-frequency cepstral coefficients) for
-    analysis
+    :param video_fp: The `video_fp` parameter is a string that represents the file path to the video
+    file from which you want to extract audio features
+    :type video_fp: str
+    :param num_frames: The `num_frames` parameter in the `extract_audio_features`
+    function represents the number of frames you want to divide the audio into for extracting audio
+    features. This parameter determines how many segments the audio will be divided into, with each
+    segment corresponding to a frame for feature extraction
     :type num_frames: int
-    :return: The function `extract_audio_features_for_each_frame` returns a list of MFCCs (Mel-frequency
-    cepstral coefficients) processed for each frame of the audio file.
+    :return: The function `extract_audio_features` returns a list of MFCC (Mel-frequency
+    cepstral coefficients) features for each frame of the input video. Each element in the list
+    corresponds to the MFCC features extracted from a single frame of the video's audio.
     """
     
     # Load audio with a specific sampling rate
@@ -53,8 +52,9 @@ def extract_audio_features_for_each_frame(audio_output_path: str, num_frames: in
 
         # Extract MFCCs for the segment
         mfccs_current_frame = librosa.feature.mfcc(y=y[start_sample:end_sample], sr=sr, n_mfcc=30)
-        mfccs_processed = np.mean(mfccs_current_frame.T, axis=0)
+        # mfccs_processed = np.mean(mfccs_current_frame.T, axis=0)
+        ## shape (number_of_sample_visual_frames, n_mfcc) with np.float32 content
         
-        mfccs_per_frame.append(mfccs_processed)
-    
+        mfccs_per_frame.append(mfccs_current_frame)
+
     return mfccs_per_frame
