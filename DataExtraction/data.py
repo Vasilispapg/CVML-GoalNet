Fixed training bug with nan values related to normalization of input image with pixels 0 intensity
Co-authored-by: Vasilis Papagrigoriou <vasilispapg@outlook.com>

DataExtraction/data.py
@@ -71,6 +71,27 @@ def extractData(video_path, anno_file, info_file,flag_to_extract):
     return return_data
 
 
+def DataAudioExtraction(video_path, anno_file, info_file):
+    video=video_path.split('/')[-1].split('.')[0] # file name, bc video_path has the extension as well (namely `filename.extension`)
+    
+    audio_features=getData('audio',video) # MFCC features. list[np.ndarray[np.float32]]. Shape (461, 128).
+        
+    if(audio_features is None):
+        # Extract data from video and save it
+        data=extractData(video_path, anno_file, info_file,[True,False,True,False])
+        # Save extracted Data
+        for d in data:
+            if d is not None:
+                if(d[0]=='frames'):
+                    frames=d[1]
+                elif(d[0]=='audio'):
+                    audio_features=d[1]
+                saveData(d[0],d[1],video)
+                
+    return audio_features
+
+    
+
 def DataExtraction(video_path, anno_file, info_file,getDataFlag=False):
     """
     Integrate visual, audio, and annotation features from a video,
@@ -95,8 +116,8 @@ def DataExtraction(video_path, anno_file, info_file,getDataFlag=False):
     # objects=getData('objects',video) # YOLO's prediction
     visual_features=getData('visual',video) # VGG16 prediction. list[list[np.ndarray[np.float32]]]. Shape (461, 2, 192).
     audio_features=getData('audio',video) # MFCC features. list[np.ndarray[np.float32]]. Shape (461, 128).
-    # title_features=getData('title',video) # TOkenized (roberta?) features
-    # encoded_objects=getData('encoded_objects',video) # ObjectEmbeddingModel features (88 features)
+    title_features=getData('title',video) # TOkenized (roberta?) features
+    encoded_objects=getData('encoded_objects',video) # ObjectEmbeddingModel features (88 features)
 
     # Poia apo afta den exeis? Kanta extract ola kai meta kanta false
     flag_to_extract=[True,True,True,True]
@@ -122,16 +143,16 @@ def DataExtraction(video_path, anno_file, info_file,getDataFlag=False):
                     visual_features=d[1]
                 elif(d[0]=='audio'):
                     audio_features=d[1]
-                # elif(d[0]=='title'):
-                #     title_features=d[1]
+                elif(d[0]=='title'):
+                    title_features=d[1]
                 
                 saveData(d[0],d[1],video)
 
-    # if(objects is None):
-    #     encoded_objects,objects = detectObjects(frames,encoded_objects=encoded_objects,video=video,tokenizer=tokenizer)
-    # else:
-    #     encoded_objects,objects = detectObjects(frames,objects,encoded_objects=encoded_objects,tokenizer=tokenizer)
+    if(objects is None):
+        encoded_objects,objects = detectObjects(frames,encoded_objects=encoded_objects,video=video,tokenizer=tokenizer)
+    else:
+        encoded_objects,objects = detectObjects(frames,objects,encoded_objects=encoded_objects,tokenizer=tokenizer)
         
-    # saveData('encoded_objects',encoded_objects,video)
+    saveData('encoded_objects',encoded_objects,video)
 
     return audio_features

model/model.py
@@ -8,7 +8,29 @@ from torch.utils.data import Dataset,DataLoader
 from time import time
 from torch.utils.data.dataset import random_split
 from torchsummary import summary
+import os
 
+
+def display_tensor_info(tnsr, var_name):
+    print('---')
+    print('variable name: %s'%(var_name))
+    print('Shape: ')
+    print(tuple(tnsr.shape))
+    print()
+    print('Mean: ')
+    print(torch.mean(tnsr).item())
+    print()
+    print('Variance: ')
+    print(torch.var(tnsr).item())
+    print()
+    print('Min, Max: ')
+    print(torch.min(tnsr).item(), torch.max(tnsr).item())
+    print()
+    print('Exists NAN')
+    print(True in torch.isnan(tnsr))
+    if True in torch.isnan(tnsr):
+        exit('ERROR')
+    print('---')
 
 # The `AudioVisualModel` class defines a neural network model that combines audio and visual features
 # extracted from MFCC and Xception models respectively, and makes a decision using softmax activation.
@@ -31,12 +53,14 @@ class AudioVisualModel(nn.Module):
             nn.LazyLinear(256),  # Adjust size calculation based on your architecture
             nn.ReLU()
         )
+        
         # OUTPUT SHAPE (22,5)
 
         # Fusion and Decision Making using softmax
         self.fusion = nn.LazyLinear(5)
 
     def forward(self, audio_input, visual_input): # audio_input: MFCC features, visual_input: Frames
+        # display_tensor_info(tnsr = visual_input, var_name = 'visual_input')
         # AUDIO FEATURES
         audio_features = self.audio_model(audio_input)
         # VISUAL FEATURES
@@ -46,21 +70,31 @@ class AudioVisualModel(nn.Module):
         visual_features = nn.ReLU(inplace=True)(xception_features)
         visual_features = F.adaptive_avg_pool2d(visual_features, (1, 1))
         visual_features = visual_features.view(visual_features.size(0), -1)
-        visual_features = nn.Linear(2048, 512)(visual_features)
-        breakpoint()
+        visual_features = nn.LazyLinear(512).to(visual_features.device)(visual_features)
         # visual_features = self.visual_model_expand(xception_features)  # Further process features with shape: [batch_size, feature_dim_visual]
-        
+
+        # FUSE
         combined_features = torch.cat((audio_features, visual_features), axis = -1)
-       
+
         output = self.fusion(combined_features)
         output = F.softmax(output, dim=1)
         return output
+
+def saveModel(model, path):
+    torch.save(model.state_dict(), path)
     
+def loadModel(model, path):
+    if(not os.path.exists(path)):
+        return None
+    model.load_state_dict(torch.load(path))
+    return model
+
 
 def callNN(sample_visual_frames, audio_features, labels):
     # labels is a list with size n_annotators, where each list contains the importance with size n_all_frames
     # Hyperparameters    
-    num_epochs = 2
+    
+    num_epochs = 1
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
 
@@ -70,12 +104,15 @@ def callNN(sample_visual_frames, audio_features, labels):
     
     dataset = DataLoaderFrameLabeled(frames = sample_visual_frames, audio = audio_features, labels = labels)
     
-    avm = AudioVisualModel().to(device)
-    # model summary
+    avm = loadModel(AudioVisualModel(), os.path.join(os.getcwd(), 'model.pth'))
+    if(avm == None):
+        print("Creating a new model")
+        avm = AudioVisualModel()
+    avm = avm.to(device)
 
     # Define the loss function and optimizer
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(avm.parameters(), lr=0.001)
+    optimizer = optim.Adam(avm.parameters(), lr=0.0002)
     
     train_dataset, val_dataset, test_dataset = splitDataset(dataset)
     
@@ -84,11 +121,9 @@ def callNN(sample_visual_frames, audio_features, labels):
     
     # Test the model
     avm = testMode(avm, test_dataset, criterion, device)        
-    exit(0)
-    
-    # Convert frames and audio features to PyTorch tensors
 
-    # print(predictions)
+    saveModel(avm, os.path.join(os.getcwd(), 'model.pth'))
+    print(f"File saved at {os.getcwd()}/model.pth")
     
 
 def trainMode(avm, train_dataset, val_dataset, num_epochs, optimizer, criterion, device="cpu"):
@@ -111,6 +146,8 @@ def trainMode(avm, train_dataset, val_dataset, num_epochs, optimizer, criterion,
             torch.cuda.empty_cache()
 
         # validation
+        print('TRAIN')
+        testMode(avm, train_dataset, criterion, device)
         print("Validation")
         avm = testMode(avm, val_dataset, criterion, device)
     return avm
@@ -169,7 +206,7 @@ def splitDataset(dataset):
 # This class `DataLoaderFrameLabeled` is a custom dataset class in PyTorch for loading labeled visual
 # frames and audio features.
 class DataLoaderFrameLabeled(Dataset):
-    def __init__(self, frames,audio, labels):
+    def __init__(self, frames, audio, labels):
         self.labels = torch.LongTensor(labels)
         self.visual_frames_tensor = torch.stack([torch.tensor(frame, dtype=torch.float).permute(2, 0, 1) for frame in frames]  )
         self.audio_features_tensor= torch.tensor(audio,dtype=torch.float)
@@ -181,7 +218,7 @@ class DataLoaderFrameLabeled(Dataset):
         # Normalize based on the Xception model's expected values
         mean = torch.tensor([0.5, 0.5, 0.5], dtype=torch.float).view(3, 1, 1)
         std = torch.tensor([0.5, 0.5, 0.5], dtype=torch.float).view(3, 1, 1)
-        return (tensor - mean) / std
+        return (tensor - mean) / (std + 0.001)
 
     def __getitem__(self, idx):
         visual_frame_tensor = self.visual_frames_tensor[idx]

videoSum/__init__.py
@@ -17,8 +17,8 @@ from knapsack import knapsack_for_video_summary
 from fscoreEval import evaluation_method
 from deletePkl import deletePKLfiles
 from videoCreator import create_video_from_frames
-from data import DataExtraction
-from model import callNN
+from data import DataAudioExtraction
+from model import callNN, display_tensor_info
 from labels import getAnnotations
 from audio import extract_audio_features_for_each_frame, extract_audio_from_video
 
@@ -32,7 +32,6 @@ video_list = [video for video in os.listdir(video_path) if video.endswith('.mp4'
 
 
 def videoSumm(annotation_path=None, info_path=None, video_path=None, summary_video_path=None,video_list=None):
-    video_list=['-esJrBWj2d8.mp4']
     for video in video_list: 
 
         # if(os.path.exists(f'{summary_video_path}{video}')):
@@ -45,14 +44,23 @@ def videoSumm(annotation_path=None, info_path=None, video_path=None, summary_vid
         original_frames=extract_frames(video_path+video, frame_rate=1)
         sample_frames=extract_frames(video_path+video)
 
+        # toIndexTheloume = -1
+        # for idx, el in enumerate(sample_frames):
+        #     if idx == 357:
+        #         print('idx:', idx)
+        #         print('el.mean:',np.mean(el))
+        #         print(el)
+        # 357 GAMIESAI
+
         # Extract Data
-        audio_features = DataExtraction(video_path+video, annotation_path, info_path,getDataFlag=getDataFlag)
+        audio_features = DataAudioExtraction(video_path+video,anno_file=annotation_path,info_file=info_path)
         # ONLY AUDIO FEATURES HERE
         
 
         labels = getAnnotations(annotation_path, video.split('.')[0])
         
         callNN(sample_frames,audio_features,labels=labels)
+        continue
         # Maping to 1/1 rate
         importance=map_scores_to_original_frames(importance, 15)
 